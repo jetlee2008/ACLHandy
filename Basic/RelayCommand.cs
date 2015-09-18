893599fb1fa23b3a49ffdf6d66c3f5ccd81623cc
jetlee2008 <jetlee_001@163.com>
2015-09-19 02:06:08 +0800

    init

---
+++ Basic/RelayCommand.cs
@@ -0,0 +1,32 @@
+﻿using ACLHandy.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ACLHandy.Basic
+{
+    public class RelayCommand : System.Windows.Input.ICommand
+    {
+        private BasicViewModel mvm;
+        private Action execute;
+        private Func<bool> canExecute;
+        public RelayCommand(BasicViewModel viewmodel, Action ex, Func<bool> canEx)
+        {
+            mvm = viewmodel;
+            this.execute = ex;
+            this.canExecute = canEx;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return canExecute();
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+            this.execute();
+        }
+    }
+}
---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,54 @@
+﻿using ACLHandy.Basic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace ACLHandy.ViewModel
+{
+    public class MainViewModel : BasicViewModel
+    {
+        public bool SignalA
+        {
+            get;
+            set;
+        }
+
+        public bool SignalB { get; set; }
+        public bool SignalC { get; set; }
+
+        public ICommand AddRecordCommand { get; set; }
+
+        public ICommand ExecuteCommand { get; set; }
+
+        public MainViewModel()
+        {
+            AddRecordCommand = new RelayCommand(this, AddRecordCommandExecute, AddRecordCommandCanExecute);
+            ExecuteCommand = new RelayCommand(this, ExecuteCommandExecute, ExecuteCommandCanExecute);
+        }
+
+        #region Command
+        public void AddRecordCommandExecute()
+        {
+
+        }
+
+        public bool AddRecordCommandCanExecute()
+        {
+            return SignalA;
+        }
+
+        public void ExecuteCommandExecute()
+        {
+            SignalA = !SignalA;
+            base.OnPropertyChanged("SignalA");
+        }
+
+        public bool ExecuteCommandCanExecute()
+        {
+            return true;
+        }
+        #endregion
+    }
+}
